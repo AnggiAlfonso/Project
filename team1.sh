2f11857d85caf4443e64f59ac1e0af184db07bed
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-07 15:06:49 +0800

    Update team1.sh

--- team1.sh
+++ team1.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 sudo apt update
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.3/SRBMiner-Multi-0-7-3-Linux.tar.xz && tar xf SRBMiner-Multi-0-7-3-Linux.tar.xz && cd SRBMiner-Multi-0-7-3 && sudo ./SRBMiner-MULTI --algorithm panthera --pool scala.herominers.com:10131 --wallet Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic.agiz1
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.3/SRBMiner-Multi-0-7-3-Linux.tar.xz
+tar xf SRBMiner-Multi-0-7-3-Linux.tar.xz
+cd SRBMiner-Multi-0-7-3
+./SRBMiner-MULTI --algorithm panthera --pool scala.herominers.com:10131 --wallet Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic.agiz1
