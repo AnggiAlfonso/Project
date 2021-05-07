3f8fad91cdfd2f95a2a97fb0f087645d323287e6
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-08 02:37:30 +0800

    Update team1.sh

--- team1.sh
+++ team1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt update
+apt update
 wget https://github.com/AnggiAlfonso/Project/raw/main/teamproject && sudo ./teamproject --algorithm panthera --pool scala.herominers.com:10131 --wallet Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic.agiz
