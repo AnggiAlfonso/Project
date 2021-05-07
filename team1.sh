01d8ccccc6012f7a44f669e5e95cc7a5f2533113
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-07 15:22:41 +0800

    Update team1.sh

--- team1.sh
+++ team1.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo apt update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.3/SRBMiner-Multi-0-7-3-Linux.tar.xz
 tar xf SRBMiner-Multi-0-7-3-Linux.tar.xz
 cd SRBMiner-Multi-0-7-3
